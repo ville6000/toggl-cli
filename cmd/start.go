8187238b9727ca541bbda655992f82083e2d529a
Ville Viklund <ville.viklund@svea.com>
2025-04-21 22:01:05 +0300

    Moved timer description to positional argument

--- cmd/start.go
+++ cmd/start.go
@@ -25,11 +25,7 @@ var startCmd = &cobra.Command{
 			log.Fatal("Missing toggl.workspace_id in config file")
 		}
 
-		description, err := cmd.Flags().GetString("description")
-		if err != nil {
-			log.Fatal("Error retrieving description flag:", err)
-		}
-
+		description := args[0]
 		projectName, err := cmd.Flags().GetString("project")
 		if err != nil {
 			log.Fatal("Error retrieving project flag:", err)
@@ -59,29 +55,18 @@ var startCmd = &cobra.Command{
 			ProjectID:   projectId,
 		}
 
-		createdEntry, err := client.CreateTimeEntry(workspaceId, timeEntry)
+		_, err = client.CreateTimeEntry(workspaceId, timeEntry)
 		if err != nil {
 			log.Println("Failed to create time entry:", err)
 			return
 		}
 
-		fmt.Printf("Created time entry: %+v\n", createdEntry)
+		fmt.Println("Timer started...")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
 
-	startCmd.Flags().StringP("description", "d", "", "Description for the time entry")
 	startCmd.Flags().StringP("project", "p", "", "Project for the time entry")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
