98b1846ffddb6af8c15ff36f73fa74660bd499d7
Ville Viklund <ville.viklund@gmail.com>
2025-04-22 21:25:50 +0300

    Add command for creating config file

---
+++ cmd/config.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Manage configuration settings",
+	Long:  "Manage configuration settings for the Toggl CLI.",
+	Run: func(cmd *cobra.Command, args []string) {
+		reader := bufio.NewReader(os.Stdin)
+
+		fmt.Print("Please enter your Toggl API token: ")
+		token, _ := reader.ReadString('\n')
+		token = token[:len(token)-1]
+
+		fmt.Print("Please enter your default workspace ID: ")
+		var workspaceID int
+		_, err := fmt.Scanf("%d", &workspaceID)
+		if err != nil {
+			log.Fatal("Invalid workspace ID:", err)
+			return
+		}
+
+		err = writeConfig(token, workspaceID)
+		if err != nil {
+			fmt.Println("Error saving configuration:", err)
+		} else {
+			fmt.Println("Configuration saved successfully!")
+		}
+	},
+}
+
+func writeConfig(token string, workspaceID int) error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("failed to get home directory: %w", err)
+	}
+	configPath := filepath.Join(home, ".toggl-cli.yaml")
+	viper.SetConfigFile(configPath)
+
+	viper.Set("toggl.token", token)
+	viper.Set("toggl.workspace_id", workspaceID)
+
+	writeErr := viper.WriteConfig()
+	if writeErr != nil {
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if errors.As(writeErr, &configFileNotFoundError) {
+			err = viper.SafeWriteConfig()
+		}
+	}
+
+	return writeErr
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+}
