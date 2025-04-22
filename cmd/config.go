6c9f7205894448074bb6b63609bc7f729b26d1b8
Ville Viklund <ville.viklund@gmail.com>
2025-04-22 21:50:57 +0300

    Handle error cases

--- cmd/config.go
+++ cmd/config.go
@@ -19,12 +19,16 @@ var configCmd = &cobra.Command{
 		reader := bufio.NewReader(os.Stdin)
 
 		fmt.Print("Please enter your Toggl API token: ")
-		token, _ := reader.ReadString('\n')
+		token, err := reader.ReadString('\n')
 		token = token[:len(token)-1]
+		if err != nil {
+			log.Fatal("Error reading input", err)
+			return
+		}
 
 		fmt.Print("Please enter your default workspace ID: ")
 		var workspaceID int
-		_, err := fmt.Scanf("%d", &workspaceID)
+		_, err = fmt.Scanf("%d", &workspaceID)
 		if err != nil {
 			log.Fatal("Invalid workspace ID:", err)
 			return
@@ -51,14 +55,21 @@ func writeConfig(token string, workspaceID int) error {
 	viper.Set("toggl.workspace_id", workspaceID)
 
 	writeErr := viper.WriteConfig()
-	if writeErr != nil {
-		var configFileNotFoundError viper.ConfigFileNotFoundError
-		if errors.As(writeErr, &configFileNotFoundError) {
-			err = viper.SafeWriteConfig()
+
+	if writeErr == nil {
+		return nil
+	}
+
+	var configFileNotFoundError viper.ConfigFileNotFoundError
+	if errors.As(writeErr, &configFileNotFoundError) {
+		err = viper.SafeWriteConfig()
+
+		if err != nil {
+			return fmt.Errorf("failed to create config file: %w", err)
 		}
 	}
 
-	return writeErr
+	return nil
 }
 
 func init() {
