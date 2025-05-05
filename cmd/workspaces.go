def039812cd7c38a806c71780c7c0d7510db451b
Ville <ville.viklund@gmail.com>
2025-05-05 16:44:51 +0300

    Reduce config fetching duplication (#21)

--- cmd/workspaces.go
+++ cmd/workspaces.go
@@ -2,10 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/ville6000/toggl-cli/internal/utils"
 	"log"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"github.com/ville6000/toggl-cli/internal/api"
 )
 
@@ -14,11 +14,7 @@ var workspacesCmd = &cobra.Command{
 	Short: "List workspaces",
 	Long:  "List all workspaces associated with the Toggl account.",
 	Run: func(cmd *cobra.Command, args []string) {
-		token := viper.GetString("toggl.token")
-		if token == "" {
-			log.Fatal("Missing toggl.token in config file")
-		}
-
+		token, _ := utils.GetTogglConfig()
 		client := api.NewAPIClient(token)
 		workspaces, err := client.GetWorkspaces()
 		if err != nil {
